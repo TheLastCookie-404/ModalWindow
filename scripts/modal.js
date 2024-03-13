021a0dfd74dd498cd18e25b1719772f7aa4467fb
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2024-03-13 21:48:55 +0600

    Modal-Js

---
+++ scripts/modal.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById("modal");
+let ShowModal = () => modal.style.display = "flex";
+let HideModal = () => modal.style.display = "none";
+
+// function ShowModal() {modal.style.display = "flex";}
+// function HideModal() {modal.style.display = "none";}
