292e84051925124000a84d864ec2385e5907541d
TheLastCookie-404 <se660745@gmail.com>
2024-03-14 13:34:51 +0500

    Modal upgraded

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,6 +1,14 @@
-const modal = document.getElementById("modal");
-let ShowModal = () => modal.style.display = "flex";
-let HideModal = () => modal.style.display = "none";
+function ShowModal(id) 
+{
+    let modal = document.getElementById(id);
+    modal.style.display = "flex";
+}
+function HideModal(id) 
+{
+    let modal = document.getElementById(id);
+    modal.style.display = "none";
+}
 
-// function ShowModal() {modal.style.display = "flex";}
-// function HideModal() {modal.style.display = "none";}
+// let ShowModal = () => modal.style.display = "flex";
+// let HideModal = () => modal.style.display = "none";
+
